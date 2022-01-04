c1a9d8425e7212ee05f7ba31710975a572022e05
prettyturtle <unifconti@gmail.com>
2022-01-04 14:16:58 +0900

    프로젝트 생성 및 SPM으로 SnapKit 추가

---
+++ PagingTabBar/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  PagingTabBar
+//
+//  Created by yc on 2022/01/04.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ PagingTabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagingTabBar
+//
+//  Created by yc on 2022/01/04.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
